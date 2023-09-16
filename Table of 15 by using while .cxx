9ca5413454d4d2da6f2f19691884c7b1dffe63f2
Forkwebtech <144381314+Forkwebtech@users.noreply.github.com>
2023-09-16 10:48:32 +0530

    Add files via upload

---
+++ Table of 15 by using while .cxx
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   int  i = 1 ;
+   printf("\n table of 15 is =  \n ");
+   
+   while (i<=10)
+   {
+   	printf("\n %d", i*15);
+   	i++;	
+   }
+  
+    printf("\n\n thanks");
+    
+    getch();
+    return 0;
+}	
